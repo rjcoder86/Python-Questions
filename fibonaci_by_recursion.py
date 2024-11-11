d7b6674ce23d4c61c9eff38e46eda60e744aec33
Rohit Jadhav <99168154+rjcoder86@users.noreply.github.com>
2024-11-11 11:14:42 +0530

    Update fibonaci_by_recursion.py

--- fibonaci_by_recursion.py
+++ fibonaci_by_recursion.py
@@ -3,3 +3,11 @@ def fibo(n,a=0,b=1):
         print(a)
         fibo(n-1,b,a+b)
 fibo(10)
+
+#without recursion
+def fibonaci(n):
+    a,b=0,1
+    
+    for i in range(n):
+        print(a)
+        a,b=b,a+b
